87fecc0e43e26b43f448fb47b90cb20a9b7897ab
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-26 18:43:20 +0800

    Pseudo code for formatting the numbers given.

---
+++ number_formatter.py
@@ -0,0 +1,5 @@
+# Prompt user for input (0-1000).
+# check if the input is a number.
+    # If not, print an error message.
+    # If it is, format the number with zeroes infront of it.
+# Print the formatted number.
