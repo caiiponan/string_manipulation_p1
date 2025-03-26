ecedb2783c2f5ca16c356e1951dad28824975aff
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-26 18:44:58 +0800

    Checking if input given is a number.

--- number_formatter.py
+++ number_formatter.py
@@ -3,5 +3,7 @@ user_input = input("Enter a number between 0 and 1000:  ")
 
 # Check if the input is a number.
     # If not, print an error message.
+if not user_input.isdigit():
+    print("Error: Please enter a number.")
     # If it is, format the number with zeroes infront of it.
 # Print the formatted number.
