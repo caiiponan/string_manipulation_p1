87d1655ebafb9c559c8037d7e68801e391ecfb2c
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-26 18:47:12 +0800

    Format numbers with zeroes if input is a number.

--- number_formatter.py
+++ number_formatter.py
@@ -6,4 +6,7 @@ user_input = input("Enter a number between 0 and 1000:  ")
 if not user_input.isdigit():
     print("Error: Please enter a number.")
     # If it is, format the number with zeroes infront of it.
+else:
+    formatted_number = "0" * (6 - len(user_input)) + user_input
+    
 # Print the formatted number.
