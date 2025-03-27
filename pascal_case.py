5e9c44e99844d2477aedff1ece33190921b73f48
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-27 23:31:58 +0800

    Convert given input into pascal case.

--- pascal_case.py
+++ pascal_case.py
@@ -2,4 +2,6 @@
 name = input("Enter your full name with incorrect casing:  ")
 
 # Convert into pascal case using title() and replace() methods.
+pascal_case = name.title().replace(" ", "")
+
 # Print the new input.
