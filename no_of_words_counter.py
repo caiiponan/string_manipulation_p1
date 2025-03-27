df5ed94a79a1b291f712179bbda64d427339fa14
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-27 23:00:18 +0800

    Find length of words using len() and split().

--- no_of_words_counter.py
+++ no_of_words_counter.py
@@ -2,4 +2,6 @@
 user_input = input("Enter a sentence:  ")
 
 # Count the number of words in the input by using len() and split() methods.
+word_count = len(user_input.split())
+
 # Print the number of words in the input.
