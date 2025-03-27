548a259183f4a383156c21043d9973289857fdd7
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-27 22:57:21 +0800

    Let user enter input sentence.

--- no_of_words_counter.py
+++ no_of_words_counter.py
@@ -1,3 +1,5 @@
 # Prompt user for input.
+user_input = input("Enter a sentence:  ")
+
 # Count the number of words in the input by using len() and split() methods.
 # Print the number of words in the input.
