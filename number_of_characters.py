f897c802967740c602f059713cdb3a975e9341ad
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-27 23:13:49 +0800

    Find character length using len() function.

--- number_of_characters.py
+++ number_of_characters.py
@@ -1,5 +1,7 @@
 # Prompt user for input.
 user_input = input("Enter a sentence: ")
 
-# Find length of input using len() method.
+# Find length of input using len() method (Including spaces).
+char_count = len(user_input)
+
 # Print the number of characters in the input.
