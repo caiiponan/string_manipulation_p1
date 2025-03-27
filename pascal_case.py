991a1708452b83d439fc61c9b4cd12c69a53b8a4
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-27 23:31:21 +0800

    Change typo.

--- pascal_case.py
+++ pascal_case.py
@@ -1,5 +1,5 @@
 # Prompt user for input.
-name = input("Enter your full name:  ")
+name = input("Enter your full name with incorrect casing:  ")
 
 # Convert into pascal case using title() and replace() methods.
 # Print the new input.
